05dc700dbddcd96d923f3907964fca8cbd29e079
菜鸟的project <30497930+Lavender2017@users.noreply.github.com>
2017-10-13 16:46:24 +0800

    Add files via upload

---
+++ picture_rename/rename.py
@@ -0,0 +1,32 @@
+# -*- coding:utf8 -*-
+
+import os
+
+class BatchRename():
+    '''
+    批量重命名文件夹中的图片文件
+
+    '''
+    def __init__(self):
+        self.path = './picture'
+
+    def rename(self):
+        filelist = os.listdir(self.path)
+        total_num = len(filelist)
+        i = 1
+        for item in filelist:
+            if item.endswith('.jpg'):
+                src = os.path.join(os.path.abspath(self.path), item)
+                dst = os.path.join(os.path.abspath('./picture_1'), '特朗普_'+str(i).rjust(5,'0')+ '.jpg')
+                try:
+                    os.rename(src, dst)
+                    print('converting %s to %s ...' % (src, dst))
+                    i = i + 1
+                except:
+                    continue
+
+        print('total %d to rename & converted %d jpgs' % (total_num, i))
+
+if __name__ == '__main__':
+    demo = BatchRename()
+    demo.rename()
